cb0cb8fd1886702394abfe4ac1f7df3c35f1a260
Jonas Clausen <jonastclausen@gmail.com>
2024-09-24 21:37:51 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Get the LAN IP address
-lan_ip=$(hostname -I | awk '{print $1}')
-
 # Backup the original config.php file
 cp /DATA/AppData/big-bear-nextcloud/html/config/config.php /DATA/AppData/big-bear-nextcloud/html/config/config.php.bak
 
-# Add the LAN IP to the config.php file
-awk -v ip="$lan_ip" '/0 => '\''localhost'\''/{print; print "    1 => '\''"ip"'\'',"; next}1' /DATA/AppData/big-bear-nextcloud/html/config/config.php.bak > /DATA/AppData/big-bear-nextcloud/html/config/config.php
+# Add the custom domain to the config.php file
+awk '
+/'trusted_domains' =>/ {
+    print;
+    print "  array (";
+    next
+}
+/),/ {
+    print "    " "3 => '\''nextcloud.huligabuliga.com'\'',"; # Adjust index as needed
+    print;
+    next
+}
+1' /DATA/AppData/big-bear-nextcloud/html/config/config.php.bak > /DATA/AppData/big-bear-nextcloud/html/config/config.php
 
 # Get the path to the docker-compose.yml file
 COMPOSE_FILE="/var/lib/casaos/apps/big-bear-nextcloud/docker-compose.yml"
